eb1a9e2b1bd8bac9aff0a7fa794a18d9ae6e7996
ice28992 <ice28992@gmail.com>
2025-10-09 22:19:33 +0900

    fix: APIキーを.envから読み込むように修正

--- src/components/ui/prefctureList/PrefectureList.tsx
+++ src/components/ui/prefctureList/PrefectureList.tsx
@@ -13,10 +13,10 @@ type PrefectureListProps = {
 
 const getPrefData = async () => {
   const response = await fetch(
-    'URL',
+    'https://yumemi-frontend-engineer-codecheck-api.vercel.app/api/v1/prefectures',
     {
       headers: {
-        'X-API-KEY': '***',
+        'X-API-KEY':  process.env.NEXT_PUBLIC_API_KEY || '',
       },
     }
   );
