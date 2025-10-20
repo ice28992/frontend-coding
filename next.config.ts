fa28f2c25d14993396204c721b73e8a7cd8c81d0
ice28992 <ice28992@gmail.com>
2025-10-20 20:33:51 +0900

    fix: next.config

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,13 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'https://frontend-engineer-codecheck-api.mirai.yumemi.io/api/:path*',
+        source: '/api/proxyPop/:prefCode',
+        destination:
+          'https://frontend-engineer-codecheck-api.mirai.yumemi.io/api/v1/population/composition/perYear?prefCode=:prefCode',
+      },
+      {
+        source: '/api/proxyPref',
+        destination: 'https://frontend-engineer-codecheck-api.mirai.yumemi.io/api/v1/prefectures',
       },
     ];
   },
